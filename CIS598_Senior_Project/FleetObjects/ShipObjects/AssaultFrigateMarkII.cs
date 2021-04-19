eb3ba56021bbeebbd1a368efb72908c1a3366d57
Jackson Carder <jgcarder@ksu.edu>
2021-04-18 22:45:09 -0500

    Working on ships

---
+++ CIS598_Senior_Project/FleetObjects/ShipObjects/AssaultFrigateMarkII.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CIS598_Senior_Project.FleetObjects.DiceObjects;
+
+namespace CIS598_Senior_Project.FleetObjects.ShipObjects
+{
+    public class AssaultFrigateMarkII : Ship
+    {
+        private int _hull;
+
+        public override int Id { get; }
+
+        public override int PointCost 
+        { 
+            get 
+            {
+                if(ShipTypeA)
+                {
+                    int x = 81;
+                    foreach(var upgd in Upgrades)
+                    {
+                        x += upgd.PointCost;
+                    }
+                    return x;
+                }
+                else
+                {
+                    int x = 72;
+                    foreach (var upgd in Upgrades)
+                    {
+                        x += upgd.PointCost;
+                    }
+                    return x;
+                }
+            } 
+        }
+
+        public override int Hull { get { return _hull; } }
+
+        public override int Command { get { return 3; } }
+
+        public override int Squadron
+        {
+            get
+            {
+                if(ShipTypeA)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 3;
+                }
+            }
+        }
+
+        public override int Engineering { get { return 4; } }
+
+        public override int[][] Movement { get; }
+        
+                                                    //{Officer, Support, Weapons, Ordinance, Offensive, Turbolasers, Ion, Defensive}
+        public override int[] UpgradeTypes { get { return new int[]{1, 0, 1, 0, 1, 1, 0, 1}; } }
+
+        public override bool HasCommander { get; set; }
+
+        public override bool ShipTypeA { get; set; } = true;
+
+        public override string Name { get { return "Assault Frigate Mark II"; } }
+
+        public override FiringArc[] Arcs { get; }
+
+        public override List<UpgradeCard> Upgrades { get; }
+
+        public override List<DefenseToken> DefenseTokens
+        {
+            get
+            {
+                //Not based on ship types
+            }
+        }
+
+        public override List<RedDie> RedAS { get; }
+
+        public override List<BlueDie> BlueAS { get; }
+
+        public override List<BlackDie> BlackAS { get; }
+
+        public AssaultFrigateMarkII(int id)
+        {
+            Id = id;
+            RedAS = new List<RedDie>();
+            BlueAS = new List<BlueDie>();
+            BlackAS = new List<BlackDie>();
+            
+        }
+    }
+}
---
+++ CIS598_Senior_Project/FleetObjects/ShipObjects/Ship.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CIS598_Senior_Project.FleetObjects.DiceObjects;
+
+namespace CIS598_Senior_Project.FleetObjects.ShipObjects
+{
+    public abstract class Ship
+    {
+
+        public abstract int Id { get; }
+
+        public abstract int PointCost { get; }
+
+        public abstract int Hull { get; }
+
+        public abstract int Command { get; }
+
+        public abstract int Squadron { get; }
+
+        public abstract int Engineering { get; }
+
+        public abstract int[][] Movement { get; }
+
+        public abstract int[] UpgradeTypes { get; }
+
+        public abstract bool HasCommander { get; set; }
+
+        public abstract bool ShipTypeA { get; set; }
+
+        public abstract string Name { get; }
+
+        public abstract FiringArc[] Arcs { get; }
+
+        public abstract List<UpgradeCard> Upgrades { get; }
+
+        public abstract List<DefenseToken> DefenseTokens { get; }
+
+        public abstract List<RedDie> RedAS { get; }
+
+        public abstract List<BlueDie> BlueAS { get; }
+
+        public abstract List<BlackDie> BlackAS { get; }
+    }
+}
